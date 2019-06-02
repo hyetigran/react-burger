09e782992df00fe8f9a4ef1b81e364349c8282d1
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-02 16:01:18 +0400

    add Button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,8 @@ const orderSummary = props => {
 			<p>A delicous burger with the following ingredients:</p>
 			<ul>{ingredientSummary}</ul>
 			<p>Continue to Checkout?</p>
+			<button>Cancel</button>
+			<button>Continue</button>
 		</Aux>
 	);
 };
