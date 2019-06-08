c0fc37aeafc71ad24ee5754908081489275ceeaa
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-08 10:31:28 +0400

    Implementing Navigation links

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,31 +87,11 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = () => {
-		// this.setState({ loading: true });
-		// const order = {
-		// 	ingredients: this.state.ingredients,
-		// 	price: this.state.totalPrice,
-		// 	customer: {
-		// 		name: 'Tigran A',
-		// 		address: {
-		// 			street: '123 teststreet',
-		// 			zipCode: '41133',
-		// 			country: 'USA'
-		// 		},
-		// 		email: 'test@test.com'
-		// 	},
-		// 	deliveryMethod: 'fastest'
-		// };
-		// axios
-		// 	.post('/orders.json', order)
-		// 	.then(response => {
-		// 		this.setState({ loading: false, purchasing: false });
-		// 	})
-		// 	.catch(error => ({ loading: false, purchasing: false }));
 		const queryParams = [];
 		for (let i in this.state.ingredients) {
 			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 		}
+		queryParams.push('price=' + this.state.totalPrice);
 		const queryString = queryParams.join('&');
 		this.props.history.push({
 			pathname: '/checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends React.Component {
 	state = {
@@ -9,19 +11,54 @@ class ContactData extends React.Component {
 		address: {
 			street: '',
 			zipCode: ''
-		}
+		},
+		loading: false
 	};
+
+	orderHandler(event) {
+		event.preventDefault();
+		this.setState({ loading: true });
+		const order = {
+			ingredients: this.props.ingredients,
+			price: this.props.price,
+			customer: {
+				name: 'Tigran A',
+				address: {
+					street: '123 teststreet',
+					zipCode: '41133',
+					country: 'USA'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'fastest'
+		};
+		axios
+			.post('/orders.json', order)
+			.then(response => {
+				this.setState({ loading: false });
+				this.props.history.push('/');
+			})
+			.catch(error => ({ loading: false }));
+	}
 	render() {
+		let form = (
+			<form>
+				<input className={classes.Input} type="text" name="name" placeholder="your name" />
+				<input className={classes.Input} type="email" name="email" placeholder="your email" />
+				<input className={classes.Input} type="text" name="street" placeholder="street" />
+				<input className={classes.Input} type="text" name="zip" placeholder="zip" />
+				<Button btnType="Success" clicked={this.orderHandler}>
+					Order
+				</Button>
+			</form>
+		);
+		if (this.state.loading) {
+			form = <Spinner />;
+		}
 		return (
 			<div className={classes.ContactData}>
 				<h4>Enter your Contact Data</h4>
-				<form>
-					<input className={classes.Input} type="text" name="name" placeholder="your name" />
-					<input className={classes.Input} type="email" name="email" placeholder="your email" />
-					<input className={classes.Input} type="text" name="street" placeholder="street" />
-					<input className={classes.Input} type="text" name="zip" placeholder="zip" />
-					<Button btnType="Success">Order</Button>
-				</form>
+				{form}
 			</div>
 		);
 	}
