6bcfcfa95430c4727f8ef6939bce1dc4c94baeea
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-08 22:12:30 +0400

    add reducer for adding and reducing ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,14 +27,14 @@ class BurgerBuilder extends Component {
 	};
 
 	componentDidMount() {
-		axios
-			.get('https://burger-7b450.firebaseio.com/ingredients.json')
-			.then(response => {
-				this.setState({ ingredients: response.data });
-			})
-			.catch(error => {
-				this.setState({ error: true });
-			});
+		// axios
+		// 	.get('https://burger-7b450.firebaseio.com/ingredients.json')
+		// 	.then(response => {
+		// 		this.setState({ ingredients: response.data });
+		// 	})
+		// 	.catch(error => {
+		// 		this.setState({ error: true });
+		// 	});
 	}
 
 	updatePurchaseState(ingredients) {
