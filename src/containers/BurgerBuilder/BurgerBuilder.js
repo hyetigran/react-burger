a79c942a817f5a399bed5b0734aa1eeb5a0dea84
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-08 22:50:53 +0400

    add redux to checkout and contact data container

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -79,17 +79,20 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = () => {
-		const queryParams = [];
-		for (let i in this.state.ingredients) {
-			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-		}
-		queryParams.push('price=' + this.state.totalPrice);
-		const queryString = queryParams.join('&');
-		this.props.history.push({
-			pathname: '/checkout',
-			search: '?' + queryString
-		});
+		this.props.history.push('/checkout');
 	};
+	// purchaseContinueHandler = () => {
+	// 	const queryParams = [];
+	// 	for (let i in this.state.ingredients) {
+	// 		queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+	// 	}
+	// 	queryParams.push('price=' + this.state.totalPrice);
+	// 	const queryString = queryParams.join('&');
+	// 	this.props.history.push({
+	// 		pathname: '/checkout',
+	// 		search: '?' + queryString
+	// 	});
+	// };
 
 	render() {
 		const disabledInfo = {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,25 +2,22 @@ import React from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import { Route } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
+import { connect } from 'react-redux';
 
 class Checkout extends React.Component {
-	state = {
-		ingredients: null,
-		price: 0
-	};
-	componentWillMount() {
-		const query = new URLSearchParams(this.props.location.search);
-		const ingredients = {};
-		let price = 0;
-		for (let param of query.entries()) {
-			if (param[0] === 'price') {
-				price = param[1];
-			} else {
-				ingredients[param[0]] = +param[1];
-			}
-		}
-		this.setState({ ingredients: ingredients, totalPrice: price });
-	}
+	// componentWillMount() {
+	// 	const query = new URLSearchParams(this.props.location.search);
+	// 	const ingredients = {};
+	// 	let price = 0;
+	// 	for (let param of query.entries()) {
+	// 		if (param[0] === 'price') {
+	// 			price = param[1];
+	// 		} else {
+	// 			ingredients[param[0]] = +param[1];
+	// 		}
+	// 	}
+	// 	this.setState({ ingredients: ingredients, totalPrice: price });
+	// }
 	checkoutCancelled = () => {
 		this.props.history.goBack();
 	};
@@ -33,19 +30,21 @@ class Checkout extends React.Component {
 		return (
 			<div>
 				<CheckoutSummary
-					ingredients={this.state.ingredients}
+					ingredients={this.props.ings}
 					checkoutCancelled={this.checkoutCancelled}
 					checkoutContinued={this.checkoutContinued}
 				/>
-				<Route
-					path={this.props.match.path + '/contact-data'}
-					render={props => (
-						<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />
-					)}
-				/>
+				<Route path={this.props.match.path + '/contact-data'} coomponent={ContactData} />
 			</div>
 		);
 	}
 }
 
-export default Checkout;
+const mapStateToProps = state => {
+	return {
+		ings: state.ingredients,
+		price: state.totalPrice
+	};
+};
+
+export default connect(mapStateToProps)(Checkout);
