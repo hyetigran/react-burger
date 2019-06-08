72864457e5d4f6962f66187a9478a82074a2fbf9
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-08 22:35:41 +0400

    enabled order now button with refactor

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,8 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
 	state = {
-		totalPrice: 4,
-		purchasable: false,
 		purchasing: false,
 		loading: false,
 		error: false
@@ -39,7 +37,7 @@ class BurgerBuilder extends Component {
 			.reduce((sum, el) => {
 				return sum + el;
 			}, 0);
-		this.setState({ purchasable: sum > 0 });
+		return sum > 0;
 	}
 
 	// addIngredientHandler = type => {
@@ -111,7 +109,7 @@ class BurgerBuilder extends Component {
 						ingredientAdded={this.props.onIngredientAdded}
 						ingredientRemoved={this.props.onIngredientRemoved}
 						disabled={disabledInfo}
-						purchasable={this.state.purchasable}
+						purchasable={this.updatePurchaseState(this.props.ings)}
 						price={this.props.price}
 						ordered={this.purchaseHandler}
 					/>
