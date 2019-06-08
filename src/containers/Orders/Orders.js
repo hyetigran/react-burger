49b3d668d4220fce815109d3d96786bcc28b2e7c
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-08 11:08:58 +0400

    fix bug

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,27 +9,26 @@ class Orders extends React.Component {
 		loading: true
 	};
 	componentDidMount() {
-		axios.get('/orders.json').then(res => {
-			const fetchedOrders = [];
-			for (let key in res.data) {
-				fetchedOrders.push({
-					...res.data[key],
-					id: key
-				});
-			}
-			this.setState({ loading: false, orders: fetchedOrders }).catch(err => {
+		axios
+			.get('/orders.json')
+			.then(res => {
+				const fetchedOrders = [];
+				for (let key in res.data) {
+					fetchedOrders.push({
+						...res.data[key],
+						id: key
+					});
+				}
+				this.setState({ loading: false, orders: fetchedOrders });
+			})
+			.catch(err => {
 				this.setState({ loading: false });
 			});
-		});
 	}
+
 	render() {
-		return (
-			<div>
-				<Order />
-				<Order />
-			</div>
-		);
+		return <div>{this.state.orders.map(order => <Order />)}</div>;
 	}
 }
 
-export default withErrorHandler(Orders);
+export default withErrorHandler(Orders, axios);
