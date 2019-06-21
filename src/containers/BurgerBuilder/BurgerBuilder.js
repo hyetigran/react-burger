c0fd2982447236e6ed9330c94b5e949f565e9b0c
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-21 19:48:18 +0400

    forwarding unauth users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -62,7 +62,11 @@ class BurgerBuilder extends Component {
 	// 	this.updatePurchaseState(updatedIngredients);
 	// };
 	purchaseHandler = () => {
-		this.setState({ purchasing: true });
+		if (this.props.isAuthenticated) {
+			this.setState({ purchasing: true });
+		} else {
+			this.props.history.push('/auth');
+		}
 	};
 
 	purchaseCancelHandler = () => {
@@ -107,6 +111,7 @@ class BurgerBuilder extends Component {
 						purchasable={this.updatePurchaseState(this.props.ings)}
 						price={this.props.price}
 						ordered={this.purchaseHandler}
+						isAuth={this.props.isAuthenticated}
 					/>
 				</Aux>
 			);
@@ -134,7 +139,8 @@ const mapStateToProps = state => {
 	return {
 		ings: state.burgerBuilder.ingredients,
 		price: state.burgerBuilder.totalPrice,
-		error: state.burgerBuilder.error
+		error: state.burgerBuilder.error,
+		isAuthenticated: state.auth.token !== null
 	};
 };
 const mapDispatchToProps = dispatch => {
